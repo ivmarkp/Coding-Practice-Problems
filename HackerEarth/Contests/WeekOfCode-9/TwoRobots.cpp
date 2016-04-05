63cb3bf9c8ba40327cc3c3435642263951e8b71a
Vivek Pal <vivekpal.dtu@gmail.com>
2016-04-05 07:08:57 +0530

    Refactor the code

--- HackerEarth/Contests/WeekOfCode-9/TwoRobots.cpp
+++ HackerEarth/Contests/WeekOfCode-9/TwoRobots.cpp
@@ -18,70 +18,62 @@ int main()
 		cin>>M>>N;
 		for(int j=0; j<N; j++)
 		{	
-			cout<<"j-loop\n\n";
-            cin>>Ma>>Mb;
-			
-            if((R1i == -1) || (abs(R1f-Ma) == 1))
-            {
-            	cout<<"if-condition\n\n";
-            	if(R2f==Ma)
-                {
-                	cout<<"Inner-If\n";
-                	if(R2i!=-1)
-                    {
-                    	R2i = R2f; 
-                    	R2f = Mb;
+            		cin>>Ma>>Mb;
+			if((R1i == -1) || (abs(R1f-Ma) == 1))
+            		{
+	            		if(R2f==Ma)
+	                	{
+	                		cout<<"Inner-If\n";
+	                		if(R2i!=-1)
+	                    		{
+	                    			R2i = R2f; 
+	                    			R2f = Mb;
 					}
 					else if(R2i==-1)
 					{
 						R2i = Ma; 
-                    	R2f = Mb;
+	                    			R2f = Mb;
 					}
-                    sum = sum + abs(R2i-Ma)+abs(Ma-Mb);
-                	break;
-                }
-                if(R1i!=-1)
-                {
-                   	R1i = R1f; 
-                   	R1f = Mb;
+	                    		sum = sum + abs(R2i-Ma)+abs(Ma-Mb);
+	                		break;
+	                	}
+	                	if(R1i!=-1)
+	                	{
+	                   		R1i = R1f; 
+	                   		R1f = Mb;
 				}
 				else if(R1i==-1)
 				{
 					R1i = Ma; 
-                   	R1f = Mb;
+	                   		R1f = Mb;
 				}
-                sum = sum + abs(R1i-Ma)+abs(Ma-Mb);
-            }	
-            else if((R2i == -1) || (abs(R2f-Ma) == 1))
-            {
-            	cout<<"else-if\n\n";
-                if(R2i!=-1)
-                {
-                   	R2i = R2f; 
-                   	R2f = Mb;
+	                	sum = sum + abs(R1i-Ma)+abs(Ma-Mb);
+            		}	
+        		else if((R2i == -1) || (abs(R2f-Ma) == 1))
+            		{
+                		if(R2i!=-1)
+                		{
+                   			R2i = R2f; 
+                   			R2f = Mb;
 				}
 				else if(R2i==-1)
 				{
 					R2i = Ma; 
-                   	R2f = Mb;
+                   			R2f = Mb;
 				}
-                sum = sum + abs(R2i-Ma)+abs(Ma-Mb);
-            }
-            else 
-            {
-            	cout<<"else\n\n";
-            	sum = 0;
+                		sum = sum + abs(R2i-Ma)+abs(Ma-Mb);
+            		}
+            		else 
+            		{
+            			sum = 0;
 			}	
-        }
-        cout<<"out of j-loop\n\n";
-        D[k] = sum;
-        k = k+1;
-    }
-    for(int l=1; l<k; l++)
-    {
-        cout<<"l-loop\n\n";
+        	}
+        	D[k] = sum;
+        	k = k+1;
+    	}
+    	for(int l=1; l<k; l++)
+    	{
 		cout<<D[l]<<"\n";
-    }
-    cout<<"out of l-loop\n";
-    return 0;
+    	}
+    	return 0;
 }
