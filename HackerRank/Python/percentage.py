0d5af225e0b92e15191dc3296efddbe7c0d4c188
Vivek Pal <vivekpal.dtu@gmail.com>
2016-04-18 23:49:58 +0530

    Update percentage.py

--- HackerRank/Python/percentage.py
+++ HackerRank/Python/percentage.py
@@ -1,7 +1,7 @@
 from __future__ import division
 n = int(raw_input())
 for i in range(0,n):
-    record{raw_input(""):marks[int(raw_input()),int(raw_input()),int(raw_input())]}
+    record{raw_input(""):marks[int(raw_input())]}
     pass
 name = raw_input("")
 average = record{name}/3
