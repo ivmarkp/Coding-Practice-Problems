ce2593d472045e442c43468ac8426cd9be8da335
Vivek Pal <vivekpal.dtu@gmail.com>
2016-04-27 04:34:54 +0530

    Update arraysum.cpp

--- HackerRank/Algorithms/arraysum.cpp
+++ HackerRank/Algorithms/arraysum.cpp
@@ -5,12 +5,13 @@
 #include <algorithm>
 using namespace std;
 
-
 int main(){
     int n;
     int sum = 0;
     cin >> n;
-    vector<int> arr(n);
+    vector<int> arr(n); 
+    /* simple array like int arr[n] will be just fine too. */
+    
     for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
        sum = sum + arr[arr_i];
