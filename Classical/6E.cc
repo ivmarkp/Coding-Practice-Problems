3bb89188fef6b30b10322f058676e8879eb8c43d
Vivek Pal <vivekpal.dtu@gmail.com>
2016-10-28 23:43:34 +0530

    Add comments

--- Classical/6E.cc
+++ Classical/6E.cc
@@ -13,10 +13,10 @@ int main()
 {
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = n-i; j >= 1; j--) 
+	for (int i = 1; i <= n; i++) { // For nos of lines
+		for (int j = n-i; j >= 1; j--) // For spaces in each line
 			cout << ' ';
-		for (int k = 1; k <= 2*i-1; k++)
+		for (int k = 1; k <= 2*i-1; k++) // For characters in each line
 			cout << '#';
 		cout << endl;
 	}
