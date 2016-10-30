9f812f3e54e6c9e599b2556a73d23ee46558f21d
Vivek Pal <vivekpal.dtu@gmail.com>
2016-10-30 19:55:05 +0530

    Search!

---
+++ Classical/7A.cc
@@ -0,0 +1,33 @@
+/* Linear Search Algorithm
+   Time Complexity : O(n)
+   
+   Input:
+   2 3 6 4 5 7
+   5
+   3
+   10
+   Output:
+   4
+   1
+   -1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(int *a, int n, int x) {
+	for (int i = 0; i < n; i++) {
+		if (x == *(a+i))
+			return i;
+	}
+	return -1;
+}
+
+int main()
+{
+	int a[] = {2, 3, 6, 4, 5, 7};
+	int n = sizeof(a)/sizeof(a[0]);
+	int x;
+	cin >> x;
+	cout << search(a, n, x) << endl;
+	return 0;
+}
---
+++ Classical/7B.cc
@@ -0,0 +1,74 @@
+/* Binary Search: Search a sorted array by repeatedly dividing the search interval in half
+   Time Complexity : O(logn)
+   
+   Input:
+   2 3 4 5 6 8 11
+   5
+   3
+   10
+   Output:
+   3
+   1
+   -1
+*/
+
+// Iterative Implementation
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> a, int n, int x) {
+	int left = 0;
+	int right = n - 1;
+	int mid = (left + right) / 2;
+	while (left <= right) {
+		if (x < a[mid]) {
+			right = mid - 1;
+			mid = (left + right) / 2;
+		}
+		else if (x > a[mid]) {
+			left = mid + 1;
+			mid = (left + right) / 2;
+		}
+		else if (x == a[mid])
+			return mid;
+	}
+	return -1;
+}
+
+int main()
+{
+	vector<int> a = {2, 3, 4, 5, 7, 8, 11};
+	int n = a.size();
+	int x;
+	cin >> x;
+	cout << search(a, n, x) << endl;
+	return 0;
+}
+
+/* Recursive Implementation
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> a, int left, int right, int x) {
+	int mid = (left + right) / 2;
+	if (right >= left) {
+		if (x < a[mid])
+			return search (a, left, mid-1, x);
+		else if (x > a[mid])
+			return search (a, mid+1, right, x);
+		else
+			return mid;
+	}
+	else
+		return -1;
+}
+
+int main()
+{
+	vector<int> a = {2, 3, 4, 5, 7, 8, 11};
+	int n = a.size();
+	int x;
+	cin >> x;
+	cout << search(a, 0, n-1, x) << endl;
+	return 0;
+}*/
