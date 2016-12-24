80724f3749d38e31d0b8ef6290a5e5b2d77036ba
Vivek Pal <vivekpal.dtu@gmail.com>
2016-12-24 17:38:49 +0530

    Add a string problem

---
+++ String/1.cc
@@ -0,0 +1,33 @@
+/* Remove all characters from an alphanumeric string.
+   E.g.
+   Input:
+   AA123bb4
+   Output:
+   1234
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string remove_char(string str) {
+    string out = "";
+    int i = 0;
+    while(str[i] != '\0') {
+        if(str[i] >= '0' && str[i] <= '9')
+            out += str[i];
+        i++;
+    }
+    out += '\0';
+    return out;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        string str;
+        cin >> str;
+        cout << remove_char(str) << endl;
+    }
+    return 0;
+}
